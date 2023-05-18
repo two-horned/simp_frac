f83b4637f3b9205d1ff135bcd3edaf7a8c35c778
Said Kadrioski <said@kadrioski.de>
2023-05-19 00:35:34 +0200

    more beautiful

--- simp_frac.py
+++ simp_frac.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 a,b = map(int, input("Enter Fraction: ").split("/"))
 def sf(a,b):
-    c=a
-    e=b%a
-    print(e)
-    while e != 0:
-        d=c
-        c=e
-        e=d%e
-
-    print(a/c,'/',b/c)
-sf(a,b)
+    c=b%a
+    while c != 0:
+        b=a
+        a=c
+        c=b%c
+    return a
+c=sf(a,b)
+print(int(a/c), '/',int(b/c))
